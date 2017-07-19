60038d9c12ef0df35fc8c2b72009ff3b0588a6b6
Rumen Rusanov <rumen.rusanov@gmail.com>
2017-07-19 14:05:57 +0300

    add missing files

---
+++ src/ng1.d.ts
@@ -0,0 +1,10 @@
+import { Reducer } from './reducer';
+import { Dispatcher } from './dispatcher';
+import { Store } from './store';
+import { State } from './state';
+export declare function _initialReducerFactory(reducer: any): any;
+export declare function _initialStateFactory(initialState: any, reducer: any): any;
+export declare function _storeFactory(dispatcher: any, reducer: any, state$: any): Store<{}>;
+export declare function _stateFactory(initialState: any, dispatcher: Dispatcher, reducer: Reducer): State<any>;
+export declare function _reducerFactory(dispatcher: any, reducer: any): Reducer;
+export declare const moduleName: any;
